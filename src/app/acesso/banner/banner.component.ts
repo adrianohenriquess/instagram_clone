e1ff0832737edaf5b96e3f586bb5a648030a30ea
adrianohenriquess <adriano.henriquesds@gmail.com>
2021-01-31 18:45:37 -0300

    Inicio da aplicação de clone do instagram

---
+++ src/app/acesso/banner/banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.css']
+})
+export class BannerComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcessoComponentComponent } from './acesso/acesso.component';
+import { BannerComponentComponent } from './acesso/banner/banner.component';
+import { LoginComponentComponent } from './acesso/login/login.component';
+import { CadastroComponentComponent } from './acesso/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcessoComponentComponent,
+    BannerComponentComponent,
+    LoginComponentComponent,
+    CadastroComponentComponent
   ],
   imports: [
     BrowserModule,
