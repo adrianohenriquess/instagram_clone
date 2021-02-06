b0f7469a0655883f4b56838c6210171b19fcb96b
adrianohenriquess <adriano.henriquesds@gmail.com>
2021-02-06 16:00:05 -0300

    Adicionando animações com key frames onde podemos adicionar animações intermediarias no tempo

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,15 +1,54 @@
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.css']
+  styleUrls: ['./acesso.component.css'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity:1 
+      })),
+      transition('void => criado', [
+        style({opacity: 0, transform: 'translate(-120px, -50px)'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ]),
+    trigger('animacao-painel', [
+      state('criado', style({
+        opacity:1 
+      })),
+      transition('void => criado', [
+        style({opacity: 0, transform: 'translate(120px, 50px)'}),
+        animate('1.5s 0s ease-in-out', keyframes([
+          style({offset: 0.15, opacity: 1, transform: 'translateX(0)'}),
+          style({offset: 0.86, opacity: 1, transform: 'translateX(0)'}),
+          style({offset: 0.88, opacity: 1, transform: 'translateY(-10px)'}),
+          style({offset: 0.90, opacity: 1, transform: 'translateY(10px)'}),
+          style({offset: 0.91, opacity: 1, transform: 'translateY(-10px)'}),
+          style({offset: 0.94, opacity: 1, transform: 'translateY(10px)'}),
+          style({offset: 1, opacity: 1, transform: 'translateX(0)'})
+        ]))
+      ])
+    ])  
+  ]
 })
 export class AcessoComponentComponent implements OnInit {
+
+  public estadoBanner: string = 'criado'; 
+
+  public estadoPainel: string = 'criado';
+
+  public cadastro: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public exibirPainel(event: string) {
+    this.cadastro = event === 'cadastro' ? true : false;
   }
 
 }
